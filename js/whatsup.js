56837738470cb2cf478d5e5a76e4cf708659d549
maneeshmohan2000 <maneeshmohan2000@gmail.com>
2017-06-27 00:01:57 +0530

    version 1.1.0 updated

--- js/whatsup.js
+++ js/whatsup.js
@@ -2,5 +2,5 @@
 	angular.module('whatsup',[]).controller('whatsupcontroller',['$scope','$rootScope', function($scope,$rootScope) {
 			$rootScope.tags = ["Sports","Weather","Academic", "News"];
 			console.log("angular initialized....");
-			console.log("whatsup upgraded version 0.0.2");
+			console.log("whatsup upgraded version 1.1.0");
 	}]);
